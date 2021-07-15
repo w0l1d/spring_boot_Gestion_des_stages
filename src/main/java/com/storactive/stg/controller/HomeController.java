246f50fb30e30cc4129897ecd14f2753ada2d82f
W0L1D <ahdoufwalid@gmail.com>
2021-07-16 00:22:43 +0100

    serverside datatable handling++ secifications for interner user
     add [Absence, Task, File] directly without going throw the inteernship page - same for internship with Interner
    
     implemented select2 for internship and interner selection
    
     LoginPageFilter handles the issues of important credentials change (username...) or disabling an interner account after he is logged in (forced log out)
    
     ++deleted select test in dashboard
    
     version heroku

--- src/main/java/com/storactive/stg/controller/HomeController.java
+++ src/main/java/com/storactive/stg/controller/HomeController.java
@@ -1,53 +1,34 @@
 package com.storactive.stg.controller;
 
 import com.storactive.stg.Utils;
-import com.storactive.stg.model.Interner;
 import com.storactive.stg.service.EmployeeService;
 import com.storactive.stg.service.InternerService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.validation.constraints.NotNull;
 
 @Controller
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class HomeController {
 
     final EmployeeService userSer;
     final InternerService internerSer;
     final BCryptPasswordEncoder encoder;
 
-    @Autowired
-    public HomeController(EmployeeService userSer,
-                          InternerService internerSer,
-                          BCryptPasswordEncoder encoder) {
-        this.userSer = userSer;
-        this.internerSer = internerSer;
-        this.encoder = encoder;
-    }
 
 
     @GetMapping({"", "home"})
     public String home(Model model) {
-        if (Utils.isAuthenticated()) {
-            model.addAttribute("interner", new Interner());
+        if (Utils.isAuthenticated())
             return "index_Dashboard";
-        }
+
         return "redirect:/login";
     }
 
-    @PostMapping({"", "home"})
-    public String postHome(Model model,
-                           @NotNull @RequestParam("interner") Integer internerId) {
-
-        return "redirect:/home";
-    }
 
 
     @GetMapping("login")
