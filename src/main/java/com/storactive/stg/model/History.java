ac23152e0657544042a1d0658beb5abfaf05ed17
W0L1D <ahdoufwalid@gmail.com>
2021-07-26 18:44:17 +0100

    Alert management started   -> internship status update alert   +++force logout on credentials changed fixed
     rebase master branch
    
     history entity changes
    
     version heroku

--- src/main/java/com/storactive/stg/model/History.java
+++ src/main/java/com/storactive/stg/model/History.java
@@ -1,8 +1,6 @@
 package com.storactive.stg.model;
 
-
 import lombok.Data;
-import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 
@@ -16,23 +14,24 @@ import static javax.persistence.TemporalType.TIMESTAMP;
 @Entity
 @Table(name = "tab_history")
 public class History {
-
-    @ManyToOne(optional = false)
-    @CreatedBy
-    protected User createdBy;
-
-    @Column(nullable = false)
-    @NotBlank
-    private String action;
-    @CreatedDate
-    @Temporal(TIMESTAMP)
-    @ColumnDefault("")
-    @Column(name = "created_at", updatable = false, nullable = false)
-    protected Date createdDate;
     @Id
     @Column(name = "id_history", updatable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(nullable = false)
+    @NotBlank
+    private String action;
+
+    @CreatedDate
+    @Temporal(TIMESTAMP)
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private Date createdDate;
+
+
+    @ManyToOne(optional = false)
+    @CreatedBy
+    private User createdBy;
 
 
 }
